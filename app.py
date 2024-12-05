5109a20bf4c8a8da54c832e78dab0ed47c89d1ca
bajajku <bajajku@sheridancollege.ca>
2024-12-05 15:10:49 -0500

    remove empty host parameter from uvicorn.run in app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from main_logic import setup_chain
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
+
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -20,7 +22,6 @@ async def get_recommendation(query: QueryRequest):
     return response
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="", port=8000)
+    uvicorn.run(app, port=8000)
 # To run the FastAPI app, run the following command in the terminal:
 # uvicorn app:app --reload
